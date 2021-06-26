ae13441f8dd006ad44a7ed18d4ab30533e4b96b5
Harbour-Dog <lee_fowlow@hotmail.com>
2021-06-25 21:45:24 -0600

    compStrat explored. Perhaps not viable.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ let compbid1, compbid2;
 let trick1, trick2;
 let yourbid1, yourbid2;
 let scorediff, trickdiff, stratmod;
-let compcards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let compcards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let compadj;
 let buttons = document.getElementsByClassName("button");
 let loop = 0;
 
@@ -17,28 +18,34 @@ document.getElementById("card3").addEventListener("click", Card3)
 document.getElementById("card4").addEventListener("click", Card4)
 document.getElementById("card5").addEventListener("click", Card5)
 document.getElementById("card6").addEventListener("click", Card6)
-document.getElementById("card7").addEventListener("click", Card7)
+document.getElementById("card7").addEventListener("click", Test)
 document.getElementById("card8").addEventListener("click", Card8)
 document.getElementById("card9").addEventListener("click", Card9)
 document.getElementById("card10").addEventListener("click", Card10)
 
 function Test(){
-    document.getElementById("scoreboard").innerHTML = "Test";
-}
+    console.log(compadj);}
 
 function trickGen(){
-    trick1 = Math.floor((Math.random() * 10) + 1);
-    trick2 = Math.floor((Math.random() * 10) + 1);
-    
-    document.getElementById("trick1").innerHTML = trick1;
+    let a = Math.floor((Math.random() * 10) + 1);
+    let b = Math.floor((Math.random() * 10) + 1);
 
-    if (trick1 != trick2){
-        document.getElementById("trick2").innerHTML = trick2;
-    } else if (trick1 == trick2 && trick2 % 2 == 0){
-        document.getElementById("trick2").innerHTML = (trick2 - 1);
-    } else if (trick1 == trick2 && trick2 % 2 != 0){
-        document.getElementById("trick2").innerHTML = (trick2 + 1);
-    }    
+    if (a > b){
+        trick1 = b;
+        trick2 = a;
+    } else if (b > a){
+        trick1 = a;
+        trick2 = b;
+    } else if (a == b && a % 2 == 0){
+         trick1 = a - 1;
+        trick2 = b;
+    } else {
+        trick1 = a;
+        trick2 = b + 1;
+    }
+
+    document.getElementById("trick1").innerHTML = trick1;    
+    document.getElementById("trick2").innerHTML = trick2;    
 
     document.getElementById("newround").disabled = true;
     
@@ -66,8 +73,18 @@ function compStrat(){
 
 function stratNorm(){
     trickdiff = Math.abs(trick1 - trick2);
+    trickavg = (trick1 + trick2)/2;
+    let a = Math.floor((Math.random() * 100) + 1);
+    let b = Math.floor(Math.random() * 2);
+    let c = Math.floor(Math.random() * 2);
+    let d = Math.floor((Math.random() * 100) + 1);
 
-
+    if (stratmod == 5 && trickdiff < 3 && trickavg < 5 && a <= 70){
+        compbid1 = b + 1;
+        compbid2 = trick2 + c;
+        compadj = compcards.splice([trick2 + c - 1], 1);
+        compadj.splice(b, 1);
+    }
 }
 
 function stratAggro(){
@@ -83,7 +100,7 @@ function stratLast(){
 }
 
 function Clear(){
-
+    
 }
 
 function Card1(){
