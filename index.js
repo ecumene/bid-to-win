a051d5b28d771fac48a18224255772b752b07522
Harbour-Dog <lee_fowlow@hotmail.com>
2021-08-01 14:33:31 -0600

    Folders rearranged. Database retrieval added, HTML connected to node

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
+app.use(express.static('./public'));
 
 app.listen(process.env.PORT, () => {
     console.log(process.env.PORT+'Step One');
@@ -17,14 +18,18 @@ const db = mysql.createPool({
     database: process.env.DATABASE
 });
 
-app.get('/testdb', (req, res) => {
-    let sql = 'CREATE DATABASE testdb';
-    db.query(sql, (err, result) => {
+//Fetches user object from database. To run on login.//
+app.get('/user/:Username', (req, res) => {
+    console.log('Fetching user with Username:' + req.params.Username);
+
+    db.query('SELECT * FROM leaderboard.user_stats WHERE Username = ?', req.params.Username, (err, rows, fields) => {
         if(err) throw err;
-        res.send(result);
+        console.log('User fetched');
+        res.json(rows);
     });
 });
 
+//Adds new user to database//
 app.put('/create_user', (req, res) => {
     let sql = 'INSERT INTO leaderboard.user_stats (Username, GP, Wins, Losses, Ties, Abandons)' +
                 'VALUES (?, 0, 0, 0, 0, 0)';
@@ -35,6 +40,7 @@ app.put('/create_user', (req, res) => {
     });
 });
 
+//Removes user from database. May not implement?//
 app.delete('/delete_user', (req, res) => {
     let sql = 'DELETE FROM leaderboard.user_stats WHERE ID=?'
     db.query (sql, req.body.ID, (err, result) => {
@@ -44,6 +50,7 @@ app.delete('/delete_user', (req, res) => {
     });
 });
 
+//Increases user's GP and Abandons by 1. To run at start of game.//
 app.put('/game_started', (req, res) => {
     let sql = 'UPDATE leaderboard.user_stats SET GP=GP+1, Abandons=Abandons+1 WHERE ID=?';
     db.query(sql, req.body.ID, (err, result) => {
@@ -53,6 +60,7 @@ app.put('/game_started', (req, res) => {
     });
 });
 
+//Increases users Wins by 1 and decreases Abandons by 1. To run after win//
 app.put('/user_win', (req, res) => {
     let sql = 'UPDATE leaderboard.user_stats SET Wins=Wins+1, Abandons=Abandons-1 WHERE ID=?';
     db.query(sql, req.body.ID, (err, result) => {
@@ -62,6 +70,7 @@ app.put('/user_win', (req, res) => {
     });
 });
 
+//Increases users Losses by 1 and decreases Abandons by 1. To run after loss//
 app.put('/user_loss', (req, res) => {
     let sql = 'UPDATE leaderboard.user_stats SET Losses=Losses+1, Abandons=Abandons-1 WHERE ID=?';
     db.query(sql, req.body.ID, (err, result) => {
@@ -71,6 +80,7 @@ app.put('/user_loss', (req, res) => {
     });
 });
 
+//Increases users Ties by 1 and decreases Abandons by 1. To run after tie//
 app.put('/user_tie', (req, res) => {
     let sql = 'UPDATE leaderboard.user_stats SET Ties=Ties+1, Abandons=Abandons-1 WHERE ID=?';
     db.query(sql, req.body.ID, (err, result) => {
--- public/index1.js
+++ public/index1.js
@@ -16,16 +16,6 @@ let scorerev = 0;
 let cbuttons = document.getElementsByClassName("cbutton");
 let buttons = document.getElementsByClassName("button");
 
-// experimental variables//
-let p1GP = 3;
-let p1ID = 2;
-let p1W = 1;
-let p1L = 2;
-let p1T = 0;
-let p1A = 0;
-let statchange = [p1ID, p1GP, p1W, p1L, p1T, p1A];
-//end of experimental variables//
-
 // variables specific to computer player //
 let scorediff, roundmod, trickdiff, trickavg;
 let compavg, youravg; 
