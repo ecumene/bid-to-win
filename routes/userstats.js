0e0167c74af224f3ec14302db2d128f2467bd4db
Harbour-Dog <lee_fowlow@hotmail.com>
2021-08-16 19:48:17 -0600

    router request for user_stats removed.

--- routes/userstats.js
+++ routes/userstats.js
@@ -10,18 +10,6 @@ const db = mysql.createPool({
     database: process.env.DATABASE
 });
 
-//Fetches user's stats to display//
-router.get('/1.0.0/:Username', (req, res) => {
-    let sql = 'SELECT * FROM leaderboard.user_stats WHERE Username=?';
-    db.query(sql, req.query.Username, (err, rows) => {
-        if(err) {throw err;
-        } else {
-        console.log('User stats retrieved.');
-        res.json(rows);
-        }
-    });    
-});
-
 //Fetches user's rank in GP in database for display. Runs off of same function as /user/:Username//
 router.get('/gprank/1.0.0/:Username', (req, res) => {
     let sql = 'SELECT * FROM leaderboard.gp_rank WHERE Username=?';
