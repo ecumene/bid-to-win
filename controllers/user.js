787763a18c02b48e8e356373466a6ce76b6f4e85
Lee Fowlow <lee_fowlow@hotmail.com>
2021-08-24 13:20:18 -0600

    more tries

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 exports.login = (req, res, next) => {
     const dbresults = () => {
-            let sql = 'SELECT * FROM leaderboard.user_stats WHERE Username=? AND Password=?';
+            let sql = 'SELECT * FROM user_stats WHERE Username=? AND Password=?';
         db.query(sql, [req.query.Username, req.query.Password], (err, rows) => {
             if(err) {throw err;
             } else {    
@@ -11,5 +11,5 @@ exports.login = (req, res, next) => {
     }
 
     res.status(200).json({Success: true, data: dbresults});
-    console.log(data);
+    console.log(dbresults);
 }
