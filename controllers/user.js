a448cd759fe8a1f1ca7d47a7013b5f6beda5cd06
Lee Fowlow <lee_fowlow@hotmail.com>
2021-08-24 13:10:45 -0600

    attempt at installing first controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,15 @@
-// exports.login = (req, res, next) => {
-//     const dbresults = () => {
-//             let sql = 'SELECT * FROM leaderboard.user_stats WHERE Username=? AND Password=?';
-//         db.query(sql, [req.query.Username, req.query.Password], (err, rows) => {
-//             if(err) {throw err;
-//             } else {    
-//             console.log('User is logged in.');
-//             res.json(rows);
-//             }
-//         });
-//     }
+exports.login = (req, res, next) => {
+    const dbresults = () => {
+            let sql = 'SELECT * FROM leaderboard.user_stats WHERE Username=? AND Password=?';
+        db.query(sql, [req.query.Username, req.query.Password], (err, rows) => {
+            if(err) {throw err;
+            } else {    
+            console.log('User is logged in.');
+            res.json(rows);
+            }
+        });
+    }
 
-//     res.status(200).json({Success: true, data: dbresults});
-//     console.log(data);
-// }
+    res.status(200).json({Success: true, data: dbresults});
+    console.log(data);
+}
