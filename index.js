04a4b447d9c195ebe4930cdebe03689703dba1ce
Lee Fowlow <lee_fowlow@hotmail.com>
2021-08-25 14:40:53 -0600

    socketPath added

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ const db = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    socketPath: '/var/lib/mysql/mysql.sock'
 });
 
 //Removes user from database. NOT YET IMPLEMENTED//
