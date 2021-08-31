d4a161704d34fef50469cb5dc42ef4e79afe9815
Lee Fowlow <lee_fowlow@hotmail.com>
2021-08-31 15:40:50 -0600

    c-log added to new syntax

--- controllers/user.js
+++ controllers/user.js
@@ -37,7 +37,8 @@ const create = (req, res, next) => {
     const dbresults = () => {
             let sql = 'SELECT * FROM leaderboard.user_stats WHERE Username=? AND Password=?';
         db.query(sql, [req.query.Username, req.query.Password], (err, result) => {
-            if(err) {throw err;
+            if(err) {
+                console.log(result);
             } else {    
             console.log('User is logged in.');
             res.status(200).json({Success: true, res: dbresults});
