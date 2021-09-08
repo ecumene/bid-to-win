18a0c2193fd793c5e0ca0db556caed799ec73c87
Lee Fowlow <lee_fowlow@hotmail.com>
2021-09-08 11:56:13 -0600

    validation re-worked

--- index.js
+++ index.js
@@ -4,21 +4,66 @@ require('dotenv').config();
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
 app.use(express.json());
 app.use(cors());
-const userstats = require('./routes/userstats.js');
-const user = require('./routes/user.js');
-console.log('in '+app.settings.env+' mode');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressHBS = require('express-handlebars');
+const {body, validationResult} = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const LocalStrat = require('passport-local').Strategy;
+
 
 app.use(express.json());
 app.use(express.static('./'));
 app.use(morgan('dev'));
+app.use(cookieParser());
+// app.set('views', path.join(__dirname, './'));//need clarification on what this does
+// app.engine('handlebars', expressHBS({defaultLayout:'layout'}));
+// app.set('view engine', 'handlebars');
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// app.use(expressVDR({//need better understanding of what is happening here
+//     errorFormatter: function(param, msg, value){
+//         let namespace = param.split('.'),
+//         root = namespace.shift(),
+//         formParam = root;
+
+//         while(namespace.length){
+//             formParam += '[' + namespace.shift() + ']';
+//         }
+//         return {
+//             param: formParam,
+//             msg: msg,
+//             value: value
+//         };
+//     }
+// }));
+
+app.use(flash());
+
+//this is to create global variables for your flash messages
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');//need this extra one because passport sets it's own error messages to this
+    next();
+});
+
+//routes handlers
+const userstats = require('./routes/userstats.js');
+const user = require('./routes/user.js');
 app.use('/user_stats', userstats); //deals with fetching user and leaderboard stats for display
 app.use('/user', user); //deals with logging in and updating user stats during games
-
-app.get('/test', (req, res) => {
-    res.send("Hello World");
-});
+console.log('in '+app.settings.env+' mode');
 
 app.listen(process.env.PORT || 31801, () => {
     console.log('Listening on Port '+ process.env.PORT);
