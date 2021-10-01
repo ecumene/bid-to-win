ab8be82776f64bbe79cf5dcfb5311de698cd4754
Lee Fowlow <lee_fowlow@hotmail.com>
2021-10-01 13:49:30 -0230

    slight cors change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 const path = require('path');
 app.use(express.json());
 app.use(cors({
-    origin: ['https://dev-me72yarl.us.auth0.com', 'https://bid-to-win.herokuapp.com']
+    origin: ['https://dev-me72yarl.us.auth0', 'https://bid-to-win.herokuapp.com']
 }
 ));
 const {auth} = require('express-openid-connect');
