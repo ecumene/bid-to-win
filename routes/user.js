67db9a65262b4efe2676eba200fcc7c5f79f1471
Lee Fowlow <lee_fowlow@hotmail.com>
2021-10-01 18:30:05 -0230

    clog added

--- routes/user.js
+++ routes/user.js
@@ -37,6 +37,7 @@ router.post('/1.0.0/create',
             const errors = validationResult(req);
             if (!errors.isEmpty()){
                 req.flash('error_msg', errors); //this is where the flash redirect has to go
+                console.log(errors);
                 return res.status(400).json({errors: errors.array()});
             } else {
                 return next();
