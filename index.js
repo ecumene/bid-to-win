eb2e6f051365f4b60c7c6717c357a08cb3998b89
Lee Fowlow <lee_fowlow@hotmail.com>
2021-10-11 14:50:43 -0230

    test again

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //routes handlers
+const database = require('./controllers/user.js');
 const userstats = require('./routes/userstats.js');
 const user = require('./routes/user.js');
 // const userController = require('../controllers/user.js');
