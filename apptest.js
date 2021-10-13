cdbea99e1b90beebfb1620aa18399fbcab57531b
Lee Fowlow <lee_fowlow@hotmail.com>
2021-10-13 16:56:39 -0230

    ?

--- apptest.js
+++ apptest.js
@@ -7,13 +7,13 @@ const db = mysql.createPool({
     database: process.env.DATABASE
 });
 
-    const app = express();
-    const router = express.Router();
-    router.use(express.json());
-    app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
-    const mysql = require('mysql');
-    require('dotenv').config();
+    // const app = express();
+    // const router = express.Router();
+    // router.use(express.json());
+    // app.use(express.json());
+    // app.use(express.urlencoded({extended: true}));
+    // const mysql = require('mysql');
+    // require('dotenv').config();
 
     const user = require('./routes/user.js');
     app.use('/user', user);
