fd60da75d243668c81b1ec4f7acfedc8dfb20ac3
Lee Fowlow <lee_fowlow@hotmail.com>
2021-10-13 17:29:12 -0230

    various changes to jest; tests added

--- routes/userstats.js
+++ routes/userstats.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const router = express.Router();
 router.use(express.json());
-const mysql = require('mysql');
 const userstatsController = require('../controllers/userstats.js');
-
-const db = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
 
 router.get('/1.0.0/gprank/:Username', userstatsController.gpRank);
 router.get('/1.0.0/winsrank/:Username', userstatsController.winsRank);
